e04e26059f12a08162c8e75d26aa713677420006
Hani Razeen <hani.razeen.20@gmail.com>
2026-05-17 17:00:47 +0530

    Update servo_test.ino

--- firmware/servo_test/servo_test.ino
+++ firmware/servo_test/servo_test.ino
@@ -1,1 +1,31 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  100
+#define SERVOMAX  500
+
+void setup() {
+  Serial.begin(115200);
+
+  pwm.begin();
+  pwm.setPWMFreq(50);
+
+  delay(1000);
+}
+
+void loop() {
+
+  // Move servo to 0 degrees
+  pwm.setPWM(0, 0, SERVOMIN);
+  delay(1000);
+
+  // Move servo to 90 degrees
+  pwm.setPWM(0, 0, 300);
+  delay(1000);
+
+  // Move servo to 180 degrees
+  pwm.setPWM(0, 0, SERVOMAX);
+  delay(1000);
+}
